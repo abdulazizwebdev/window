d844f58a0f8d2ee69bb6ac24cae49cde59805b42
abdulaziz <abdulazizabduvahobov3655@gmail.com>
2020-09-25 00:38:02 +0300

    modals.js was modified

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,24 +1,31 @@
 const modals = () => {
-    function bindModals(triggerSelector, modalSelector, closeSelector) {
+    function bindModals(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
-            close  = document.querySelector(closeSelector);
+            close  = document.querySelector(closeSelector),
+            windows = document.querySelectorAll('[data-modal]');
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
                 if (e.target) {
                     e.preventDefault();
                 }
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
                 openModal(modal);
             });
         });
 
         close.addEventListener('click', () => {
             closeModal(modal);
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
         } );
 
         modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
+            if (e.target === modal && closeClickOverlay) {
                 closeModal(modal);
             }
         });
@@ -43,6 +50,9 @@ const modals = () => {
 
     bindModals('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModals('.phone_link', '.popup', '.popup .popup_close');
+    bindModals('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    bindModals('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+    bindModals('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
     // showModalByTime('.popup', 60000);
 };
 
